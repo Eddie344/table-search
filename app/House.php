e496c90aa03174123bfd11ca6e22f22cd410c9c4
Эд <eduard.bazhenov34@gmail.com>
2020-05-06 00:58:26 +0300

    model, migration, seeder are done

---
+++ app/House.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class House extends Model
+{
+    //Disable timestamps fields as we do not use it
+    public $timestamps = false;
+
+
+}
